8cc035fcef24099b5c7bb900d1ce96f418ec8fa5
rockingaditya <68543352+rockingaditya@users.noreply.github.com>
2020-08-09 07:12:24 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,104 @@
+var p5Inst = new p5(null, 'sketch');
+
+window.preload = function () {
+  initMobileControls(p5Inst);
+
+  p5Inst._predefinedSpriteAnimations = {};
+  p5Inst._pauseSpriteAnimationsByDefault = false;
+  var animationListJSON = {"orderedKeys":["a6870703-0124-47f7-acff-dbe905f5014c","5ce44e39-12ac-4a66-88cf-a87a0ed6a180","33841f90-7a53-4346-b956-e51d1961959b"],"propsByKey":{"a6870703-0124-47f7-acff-dbe905f5014c":{"name":"monkey","sourceUrl":null,"frameSize":{"x":560,"y":614},"frameCount":10,"looping":true,"frameDelay":12,"version":"xv7jmJ36u8Dvul3Esvc2F4xc_swICWUb","loadedFromSource":true,"saved":true,"sourceSize":{"x":1680,"y":1842},"rootRelativePath":"assets/a6870703-0124-47f7-acff-dbe905f5014c.png"},"5ce44e39-12ac-4a66-88cf-a87a0ed6a180":{"name":"Banana","sourceUrl":"assets/v3/animations/0Pmc2UypwJxUUUBBxMOOYmiSvh97BJLRo_BQZbjyEto/5ce44e39-12ac-4a66-88cf-a87a0ed6a180.png","frameSize":{"x":1080,"y":1080},"frameCount":1,"looping":true,"frameDelay":4,"version":"6T72D_ExA5KksbgZSXpI_jaYe01EKoMO","loadedFromSource":true,"saved":true,"sourceSize":{"x":1080,"y":1080},"rootRelativePath":"assets/v3/animations/0Pmc2UypwJxUUUBBxMOOYmiSvh97BJLRo_BQZbjyEto/5ce44e39-12ac-4a66-88cf-a87a0ed6a180.png"},"33841f90-7a53-4346-b956-e51d1961959b":{"name":"Stone","sourceUrl":"assets/v3/animations/0Pmc2UypwJxUUUBBxMOOYmiSvh97BJLRo_BQZbjyEto/33841f90-7a53-4346-b956-e51d1961959b.png","frameSize":{"x":512,"y":512},"frameCount":1,"looping":true,"frameDelay":4,"version":"Aq5.D7lH7kZ_1pbxcmf..Bo5X3Uxtw3y","loadedFromSource":true,"saved":true,"sourceSize":{"x":512,"y":512},"rootRelativePath":"assets/v3/animations/0Pmc2UypwJxUUUBBxMOOYmiSvh97BJLRo_BQZbjyEto/33841f90-7a53-4346-b956-e51d1961959b.png"}}};
+  var orderedKeys = animationListJSON.orderedKeys;
+  var allAnimationsSingleFrame = false;
+  orderedKeys.forEach(function (key) {
+    var props = animationListJSON.propsByKey[key];
+    var frameCount = allAnimationsSingleFrame ? 1 : props.frameCount;
+    var image = loadImage(props.rootRelativePath, function () {
+      var spriteSheet = loadSpriteSheet(
+          image,
+          props.frameSize.x,
+          props.frameSize.y,
+          frameCount
+      );
+      p5Inst._predefinedSpriteAnimations[props.name] = loadAnimation(spriteSheet);
+      p5Inst._predefinedSpriteAnimations[props.name].looping = props.looping;
+      p5Inst._predefinedSpriteAnimations[props.name].frameDelay = props.frameDelay;
+    });
+  });
+
+  function wrappedExportedCode(stage) {
+    if (stage === 'preload') {
+      if (setup !== window.setup) {
+        window.setup = setup;
+      } else {
+        return;
+      }
+    }
+// -----
+
+var monkey = createSprite(70,350,20,20);
+monkey.scale = 0.2;
+monkey.setAnimation("monkey");
+var ground = createSprite(200,390,400,10);
+ground.visible = false;
+monkey.collide(ground);
+
+var survivaltime = 0;
+
+stroke =("black");
+textSize(20);
+fill("black");
+
+
+
+
+
+function draw() {
+
+  background(255);
+  survivalTime=Math.round(World.frameRate);
+  text("survival Time: "+survivalTime,100,50);
+  
+
+  bananas();
+  obstacles();
+  drawSprites();
+}
+
+function bananas(){
+  if(World.frameCount %90 === 0){
+ var banana = createSprite(400,220,10,20);
+  banana.setAnimation("Banana");
+  banana.velocityX = -3;
+  banana.scale =0.08;
+}
+}
+function obstacles(){
+  if(World.frameCount%90 ===0){
+    var obstacle = createSprite(400,390,10,10);
+    obstacle.setAnimation("Stone");
+    obstacle.velocityX = -4;
+    obstacle.scale = 0.2;
+  }
+}
+
+
+
+  
+
+// -----
+    try { window.draw = draw; } catch (e) {}
+    switch (stage) {
+      case 'preload':
+        if (preload !== window.preload) { preload(); }
+        break;
+      case 'setup':
+        if (setup !== window.setup) { setup(); }
+        break;
+    }
+  }
+  window.wrappedExportedCode = wrappedExportedCode;
+  wrappedExportedCode('preload');
+};
+
+window.setup = function () {
+  window.wrappedExportedCode('setup');
+};
